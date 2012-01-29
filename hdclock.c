2342f1ed424ba0165487f5f613f30b296660ea86
Stefan Tomanek <stefan@pico.ruhr.de>
2012-01-29 04:09:11 +0100

    implement multiple animations

--- hdclock.c
+++ hdclock.c
@@ -9,10 +9,13 @@
 #define P_OFFSET (1*(PMAX+1)/8)
 #define P_CENTER (3*(PMAX/8))
 
+typedef uint8_t(*display_t)(uint8_t);
+
 static volatile uint8_t OVERFLOW = 0;
+static volatile uint8_t ANIMATION_PHASE = 0;
 
 static uint8_t display_magic_eye(uint8_t pos) {
-	uint8_t width = 10;
+	uint8_t width = ANIMATION_PHASE;
 	uint8_t left = (uint16_t)P_CENTER+(width/2)%PMOD;
 	uint8_t right = (uint16_t)P_CENTER+PMOD-(width/2)%PMOD;
 	if (pos < left && pos > right) {
@@ -24,7 +27,7 @@ static uint8_t display_magic_eye(uint8_t pos) {
 
 static uint8_t display_radar(uint8_t pos) {
 	uint8_t width = 10;
-	uint8_t start = 0;
+	uint8_t start = ANIMATION_PHASE;
 	uint8_t end = ((uint16_t)start+width)%PMOD;
 	if (pos > start && pos < end) {
 		return 1;
@@ -34,7 +37,7 @@ static uint8_t display_radar(uint8_t pos) {
 }
 
 static uint8_t display_progress(uint8_t pos) {
-	uint8_t progress = 0;
+	uint8_t progress = ANIMATION_PHASE;
 	if (pos < progress) {
 		return 1;
 	} else {
@@ -45,6 +48,14 @@ static uint8_t display_progress(uint8_t pos) {
 static uint8_t display_half(uint8_t pos) {
 	return pos < PMAX/2;
 }
+
+#define N_DISPLAYS 3
+static display_t display[N_DISPLAYS] = {
+	&display_progress,
+	&display_radar,
+	&display_magic_eye,
+};
+
 
 static uint16_t getCounter(void) {
 	uint16_t result = (((uint16_t)OVERFLOW)<<8) | TCNT0;
@@ -68,14 +79,14 @@ int main(void) {
 	PORTB |= (1<<PB4);
 
 	TCCR0B |= (1<<CS02 | 0<<CS01 | 0<<CS00);
-	TIMSK |= (1<<TOIE0);
-	GIMSK |= (1 <<PCIE);
+	TCCR1 |= (1<<CS13 | 1<<CS12 | 0<<CS11 | 0<<CS10);
+	TIMSK |= (1<<TOIE0 | 1<<TOIE1);
+	GIMSK |= (1<<PCIE);
 	PCMSK |= (1<<PCINT4);
 
 	uint8_t pos = 0;
 
-	uint8_t (*display)(uint8_t) = 0;
-	display = &display_half;
+	uint8_t display_i = 0;
 
 	// test LEDs
 	PORTB &= ~(1<<PB3);
@@ -84,11 +95,15 @@ int main(void) {
 	sei();
 	while (1) {
 		if (!duration) continue;
-		if (!display) continue;
+
+		if (ANIMATION_PHASE == 0) {
+			display_i++;
+			display_i %= N_DISPLAYS;
+		}
 
 		// where are we exactly?
 		pos = PMOD-( ((((((uint32_t)getCounter())<<16) / duration)*PMAX)>>16)+P_OFFSET ) % (PMOD);
-		if ((*display)(pos)) {
+		if (display[display_i](pos)) {
 			PORTB &= ~(1<<PB3);
 		} else {
 			PORTB |= 1<<PB3;
@@ -98,6 +113,10 @@ int main(void) {
 
 ISR(TIM0_OVF_vect) {
 	OVERFLOW++;
+}
+
+ISR(TIM1_OVF_vect) {
+	ANIMATION_PHASE++;
 }
 
 ISR(PCINT0_vect) {
